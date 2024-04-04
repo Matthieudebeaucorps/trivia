828e4724720217f84b4464d773599bc35d3a2cf5
Matthieu de Beaucorps <matthieubeaucorps@gmail.com>
2024-04-04 19:18:11 +0100

    first comit

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 import pandas as pd
 import os
 import random
 
 app = Flask(__name__)
+app.secret_key = 'abc124'  # Your chosen secret key
 
 categories_dir = '/Users/matthieudebeaucorps/Desktop/Projects/OpenTriviaQA/categories csv'
 categories = ["animals", "geography", "history", "literature", "movies", "music", "science-technology", "sports", "video-games", "world"]
@@ -15,25 +16,38 @@ def load_question(category):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        session['teams'] = {
+            'team1': request.form.get('team1', ''),
+            'team2': request.form.get('team2', ''),
+            'team3': request.form.get('team3', ''),
+            'team4': request.form.get('team4', ''),
+        }
+        return redirect(url_for('choose_category'))
     return render_template('index.html', categories=categories)
+
+@app.route('/choose_category')
+def choose_category():
+    if 'teams' not in session or all(value == '' for value in session['teams'].values()):
+        # Redirect to index if teams not set
+        return redirect(url_for('index'))
+    return render_template('choose_category.html', categories=categories)
 
 @app.route('/question', methods=['GET', 'POST'])
 def question():
     category = request.args.get('category')
     if not category or category not in categories:
-        return redirect(url_for('index'))
+        return redirect(url_for('choose_category'))
     
     question_row = load_question(category)
     if request.method == 'POST':
+        team_key = request.form.get('team')
         user_answer = request.form.get('answer')
         correct_answer = question_row['Correct']
         feedback = "Correct!" if user_answer == correct_answer else "Incorrect!"
         return render_template('answer.html', feedback=feedback, correct_answer=correct_answer, question_row=question_row)
     
-    return render_template('question.html', question_row=question_row)
+    return render_template('question.html', question_row=question_row, teams=session.get('teams'))
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
